2bee37186878179694b41a4e4ba3c3acad86a69d
AlexL67150 <49148244+AlexL67150@users.noreply.github.com>
2019-04-02 00:43:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', function (message) 
+{
+    if (message.content.includes ("AkBot"))
+    {
+        message.channel.send('On parle de moi ?! :stuck_out_tongue_winking_eye:')
+    }
+})
+
+bot.on('message', function (message) 
+{
+    if (message.content.includes ("Akbot"))
+    {
+        message.channel.send('On parle de moi ?! :stuck_out_tongue_winking_eye:')
+    }
+})
+
+bot.on('message', function (message) 
+{
+    if (message.content.includes ("akbot"))
+    {
+        message.channel.send('On parle de moi ?! :stuck_out_tongue_winking_eye:')
+    }
+})
+
+bot.on('message', function (message) 
+{
+    if (message.content.includes ("Collection Day started!"))
+    {
+        bot.channels.get('501324980861272066').send('<@&561304424212070431> : GDC Clash Royale, le jour de collection commence !!!');
+    }
+})
+
+bot.on('message', function (message) 
+{
+    if (message.content.includes ("New war started!"))
+    {
+        bot.channels.get("501324980861272066").send('<@&561304424212070431> : GDC Clash Royale, la guerre est déclarée !!!');    
+    }
+})
+
+
+bot.login(process.env.TOKEN);
